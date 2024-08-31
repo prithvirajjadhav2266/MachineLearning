e2fd14214d3c7c218d201204c7df736e3c3084c3
prithvirajjadhav2266 <122175245+prithvirajjadhav2266@users.noreply.github.com>
2024-09-01 00:47:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,16 @@ st.title("🤖Machine Learning App")
 
 st.info("Tis app generates the results and visualizations for the datasets")
 
-with st.expander("Data"):
+with st.expander("Dataset working on"):
   st.write("**Raw Data**")
   data=pd.read_csv("https://raw.githubusercontent.com/prithvirajjadhav2266/MachineLearning/main/penguin_cleaned.csv")
   data
 
+  st.write("**Variables : X**")
+  X=data.drop("species",axis=1)
+  X
+
+  st.write("**Variables : y**")
+  y=data.species
+  y
 #the species is the target variable (Y)  and remaining variables are the input variables (X)
