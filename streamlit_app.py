0978185406beaa87c659f9a1dab03cf2fbc014ba
prithvirajjadhav2266 <122175245+prithvirajjadhav2266@users.noreply.github.com>
2024-09-01 00:12:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title("Machine Learning App")
+st.title("🤖Machine Learning App")
 
 st.write("Hello World!")
+
+st.info("Tis app generates the results and visualizations for the datasets")
