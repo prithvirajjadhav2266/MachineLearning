985fa07c59cb5b7eaf67e95367a3e663ea292f05
prithvirajjadhav2266 <122175245+prithvirajjadhav2266@users.noreply.github.com>
2024-09-01 00:25:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.title("🤖Machine Learning App")
 
 st.info("Tis app generates the results and visualizations for the datasets")
 
-data=pd.read_csv("https://github.com/prithvirajjadhav2266/MachineLearning/blob/main/penguin_cleaned.csv")
+data=pd.read_csv("https://raw.githubusercontent.com/prithvirajjadhav2266/MachineLearning/main/penguin_cleaned.csv")
 
 data
