fa346cf621f6070083c18fe34f6eb47ecb089d40
prithvirajjadhav2266 <122175245+prithvirajjadhav2266@users.noreply.github.com>
2024-09-01 10:44:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import matplotlib.pyplot as plt
+
 from sklearn.ensemble import RandomForestClassifier
 
 st.title("🤖 Machine Learning App")
