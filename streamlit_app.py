661c600232aae44a6e2f9e829c5c61ccebb674b5
prithvirajjadhav2266 <122175245+prithvirajjadhav2266@users.noreply.github.com>
2024-09-01 10:27:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,5 @@ model.fit(X,y)
 prediction=model.predict(input_row)
 prediction_probability=model.predict_proba(input_row)
 data_prediction_probablity=pd.DataFrame(prediction_probability)
-data_prediction_probability.rename(columns={0:"Adelie",
-                                       1:"Chinstrap",
-                                       2:"Gentoo"})
-prediction_probability
+data_prediction_probability.columns=["Adelie","Chinstrap","Gentoo"]
+data_prediction_probability
